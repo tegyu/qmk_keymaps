c7f898f4e54fff199dcd0c5d0f39dfbc2b646954
Euisoon Ahn <euisoon.ahn@gmail.com>
2022-02-04 15:11:29 +0900

    initial

--- keymap.c
+++ keymap.c
@@ -5,35 +5,27 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // 0: Base Layer
   [0] = LAYOUT_esnahn( \
-      KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          \
-      KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, LT(11,KC_BSLS),   \
-      KC_GRV,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           \
-      KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),  KC_UP,   KC_DEL,  \
-      KC_LCTL,     KC_LGUI, KC_LALT,          LT(10, KC_LANG1), KC_SPC,                    KC_HOME, KC_END,  KC_LCTL, KC_LEFT, KC_DOWN, KC_RIGHT \
+      KC_GESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          \
+      KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          \
+      KC_LNG1,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           \
+      KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),  KC_UP,   KC_DEL,  \
+      KC_LCTL,      KC_LGUI, KC_LALT,          KC_SPC, LT(10, KC_SPC),                      KC_CAPS, KC_LNG2, KC_QUES, KC_LEFT, KC_DOWN, KC_RIGHT \
   ),    
-  // 1: Plain Layer 
-  [1] = LAYOUT_esnahn( \
-      _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
-      _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
-      _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          \
-      _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-      _______,     _______, _______,          KC_SPC,           KC_SPC,                    KC_LANG1,_______, _______, _______, _______, _______  \
-  ),
-  // 10: Left space Layer
+  // // 1: Plain Layer 
+  // [1] = LAYOUT_esnahn( \
+  //     _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
+  //     _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
+  //     _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          \
+  //     _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+  //     _______,     _______, _______,          KC_SPC,           KC_SPC,                    KC_LANG1,_______, _______, _______, _______, _______  \
+  // ),
+  // 10: right space Layer
   [10] = LAYOUT_esnahn( \
-      _______,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          \
-      _______,     _______, _______, _______, _______, C(KC_T), _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______,          \
-      _______,     C(KC_A), C(KC_S), _______, C(KC_F), _______, _______, _______, _______, _______, _______, _______,          _______,          \
-      _______,              C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_INS,  \
-      _______,     _______, _______,          _______,          _______,                   _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
-  ),
-  // 11: Backslash Layer
-  [11] = LAYOUT_esnahn( \
-      _______,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          \
+      KC_GRV,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          \
       _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______,          \
-      KC_CAPS,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          \
-      _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_INS,  \
-      _______,     _______, _______,          TG(1),            _______,                   _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
+      _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          \
+      _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_QUES,          KC_PGUP, _______, \
+      _______,     _______, _______,          _______,          _______,                   _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
   ),
 
 /*   // 15: Transparent Layer Template
@@ -48,20 +40,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-// Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
-const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 2, HSV_RED},       // Light 2 LED(s), starting with LED 0
-    {RGBLED_NUM-2, 2, HSV_RED}       // Light 2 LED(s), finishing with the last LED
-);
+// // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
+// const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 2, HSV_RED},       // Light 2 LED(s), starting with LED 0
+//     {RGBLED_NUM-2, 2, HSV_RED}       // Light 2 LED(s), finishing with the last LED
+// );
 // Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
 const rgblight_segment_t PROGMEM my_spacebar_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0+1, RGBLED_NUM/2-2, HSV_WHITE}
+    {0, RGBLED_NUM, HSV_GOLD}
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_capslock_layer,
-    my_spacebar_layer    // Overrides caps lock layer
+    my_spacebar_layer
 );
 
 
@@ -120,7 +111,7 @@ bool led_update_user(led_t led_state) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Callback for layer functions, for users.
 
-    // lights up when layer 1 (plain spacebar and lang key) is set
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+    // lights up rgb layer 0 when key layer 10 is set
+    rgblight_set_layer_state(0, layer_state_cmp(state, 10));
     return state;
 }
